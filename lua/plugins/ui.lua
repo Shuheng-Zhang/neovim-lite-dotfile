15e944b9018db61b75f48673ee44715a1a6d7d11
Shuheng-Zhang <zhangshuheng01@163.com>
2025-04-02 18:31:58 +0800

    重构UI配置

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,19 @@
+return {
+	-- colorscheme
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				background = {
+					light = "mocha",
+					dark = "mocha",
+				},
+			})
+			vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
+}
