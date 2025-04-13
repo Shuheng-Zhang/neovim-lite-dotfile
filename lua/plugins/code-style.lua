3be307552ab9995007f1a3b4a04663010e666e59
Shuheng-Zhang <zhangshuheng01@163.com>
2025-04-13 20:25:45 +0800

    Improve code fortmatting

--- lua/plugins/code-style.lua
+++ lua/plugins/code-style.lua
@@ -53,4 +53,18 @@ return {
 			require("illuminate").configure({ delay = 200 })
 		end,
 	},
+	-- code formating
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 3000,
+				lsp_format = "fallback",
+			},
+		},
+	},
 }
