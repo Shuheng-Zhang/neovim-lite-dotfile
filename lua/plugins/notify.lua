0da797e76c641f39bcc6330540513b60293a55ac
Shuheng-Zhang <zhangshuheng01@163.com>
2025-05-20 09:34:41 +0800

    Impl notification

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,7 +2,26 @@ return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
 	opts = {
-		-- add any options here
+		lsp = {
+			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+			},
+		},
+		presets = {
+			lsp_doc_border = true,
+		},
+		routes = {
+			{
+				filter = {
+					event = "notify",
+					find = "No information available",
+				},
+				opts = { skip = true },
+			},
+		},
 	},
 	dependencies = {
 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
