25fd973ccd5950dbe27929e45754c99be3d8c5c7
Shuheng-Zhang <zhangshuheng01@gmail.com>
2025-05-31 00:45:19 +0800

    Improve appearance

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -112,6 +112,11 @@ return {
 				offsets = {
 					{ filetype = "neo-tree", text_align = "center", separator = false },
 				},
+				diagnostics = "nvim_lsp",
+				diagnostics_indicator = function(count, level)
+					local icon = level:match("error") and " " or " "
+					return " " .. icon .. count
+				end,
 			},
 		},
 	},
