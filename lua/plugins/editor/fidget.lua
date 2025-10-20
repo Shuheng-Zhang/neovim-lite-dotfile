2fca19330a59b67403341226982f2624b5bbaa9c
Shuheng-Zhang <zhangshuheng01@163.com>
2025-10-20 11:09:17 +0800

    feat: add fidget.nvim transparent theme supports

--- lua/plugins/editor/fidget.lua
+++ lua/plugins/editor/fidget.lua
@@ -1,3 +1,21 @@
+vim.api.nvim_set_hl(0, "FidgetText", { fg = "#aaaaaa", bg = "none" })
+vim.api.nvim_set_hl(0, "FidgetTitle", { fg = "#aaaaaa", bg = "none" })
+vim.api.nvim_set_hl(0, "FidgetTask", { fg = "#aaaaaa", bg = "none" })
+
 return {
-  { "j-hui/fidget.nvim", event = "LspAttach", opts = {} }
+	{
+		"j-hui/fidget.nvim",
+		event = "LspAttach",
+		config = function()
+			require("fidget").setup({
+				notification = {
+					window = {
+						normal_hl = "FidgetText", -- 使用自定义高亮组
+						winblend = 0, -- 不透明
+						border = "none", -- 去掉边框
+					},
+				},
+			})
+		end,
+	},
 }
