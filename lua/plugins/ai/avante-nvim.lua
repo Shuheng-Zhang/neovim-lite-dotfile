4feac43060c2c8caa91d53829b3423e92faecf19
Shuheng-Zhang <zhangshuheng01@163.com>
2025-10-24 16:32:34 +0800

    fix: decrease notifications

--- lua/plugins/ai/avante-nvim.lua
+++ lua/plugins/ai/avante-nvim.lua
@@ -36,7 +36,7 @@ return {
 						embed_image_as_base64 = false,
 						prompt_for_file_name = false,
 						drag_and_drop = {
-							insert_mode = true,
+							insert_mode = false,
 						},
 						-- Windows 用户必需
 						use_absolute_path = true,
