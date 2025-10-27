6b1aed8a2d68ad2ca7a42f1c910e02f99161272d
Shuheng-Zhang <zhangshuheng01@163.com>
2025-10-27 10:34:22 +0800

    feat: customize tab indcator color

--- lua/plugins/appearance/tab-bar.lua
+++ lua/plugins/appearance/tab-bar.lua
@@ -2,22 +2,51 @@ return {
 	{
 		"akinsho/bufferline.nvim",
 		version = "*",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		opts = {
-			options = {
-				mode = "buffers",
-				offsets = {
-					{ filetype = "neo-tree", text_align = "center", separator = false },
-				},
-				diagnostics = "nvim_lsp",
-				diagnostics_indicator = function(count, level)
-					local icon = level:match("error") and " " or " "
-					return " " .. icon .. count
-				end,
-				indicator = {
-					style = "underline",
-				},
-			},
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"catppuccin/nvim",
 		},
+		config = function()
+			local mocha = require("catppuccin.palettes").get_palette("mocha")
+
+			require("bufferline").setup({
+				options = {
+					mode = "buffers",
+					offsets = {
+						{ filetype = "neo-tree", text_align = "center", separator = false },
+					},
+					diagnostics = "nvim_lsp",
+					diagnostics_indicator = function(count, level)
+						local icon = level:match("error") and " " or " "
+						return " " .. icon .. count
+					end,
+					indicator = {
+						style = "underline",
+					},
+					separator_style = { "", "" }, -- important
+				},
+				highlights = {
+					indicator_selected = {
+						fg = mocha.peach,
+						bg = "none",
+						sp = mocha.peach,
+						underline = true,
+					},
+					buffer_selected = {
+						fg = mocha.peach,
+						bg = "NONE",
+						bold = true,
+						underline = true,
+						sp = mocha.peach,
+					},
+					close_button_selected = {
+						fg = mocha.red,
+						bg = "NONE",
+						underline = true,
+						sp = mocha.peach,
+					},
+				},
+			})
+		end,
 	},
 }
